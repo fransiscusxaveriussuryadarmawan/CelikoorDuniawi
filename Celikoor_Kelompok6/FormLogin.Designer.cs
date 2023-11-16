bfed45ac5f7bd78ecbbba871e413fdc74c2f0a43
fx surya darmawan <fxsurya27@gmail.com>
2023-11-16 21:38:15 +0700

    GANTI NAMA PROJECT

---
+++ Celikoor_Kelompok6/FormLogin.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Celikoor_Kelompok6
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.linkRegister = new System.Windows.Forms.LinkLabel();
+            this.buttonKembali = new System.Windows.Forms.Button();
+            this.buttonMasuk = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPass = new System.Windows.Forms.Label();
+            this.textBoxUname = new System.Windows.Forms.TextBox();
+            this.labelUname = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(493, 61);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Celikoor 21 Cineplex";
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLogo.BackgroundImage = global::Celikoor_Kelompok6.Properties.Resources.logo;
+            this.pictureBoxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxLogo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(30, 12);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxLogo.TabIndex = 18;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // linkRegister
+            // 
+            this.linkRegister.AutoSize = true;
+            this.linkRegister.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkRegister.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.linkRegister.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.linkRegister.Location = new System.Drawing.Point(541, 329);
+            this.linkRegister.Name = "linkRegister";
+            this.linkRegister.Size = new System.Drawing.Size(177, 24);
+            this.linkRegister.TabIndex = 17;
+            this.linkRegister.TabStop = true;
+            this.linkRegister.Text = "Don\'t have account?";
+            // 
+            // buttonKembali
+            // 
+            this.buttonKembali.AutoSize = true;
+            this.buttonKembali.BackColor = System.Drawing.Color.Tan;
+            this.buttonKembali.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKembali.Location = new System.Drawing.Point(630, 383);
+            this.buttonKembali.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonKembali.Name = "buttonKembali";
+            this.buttonKembali.Size = new System.Drawing.Size(135, 48);
+            this.buttonKembali.TabIndex = 16;
+            this.buttonKembali.Text = "Kembali";
+            this.buttonKembali.UseVisualStyleBackColor = false;
+            this.buttonKembali.Click += new System.EventHandler(this.buttonKembali_Click);
+            // 
+            // buttonMasuk
+            // 
+            this.buttonMasuk.AutoSize = true;
+            this.buttonMasuk.BackColor = System.Drawing.Color.Tan;
+            this.buttonMasuk.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMasuk.Location = new System.Drawing.Point(30, 383);
+            this.buttonMasuk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonMasuk.Name = "buttonMasuk";
+            this.buttonMasuk.Size = new System.Drawing.Size(107, 48);
+            this.buttonMasuk.TabIndex = 15;
+            this.buttonMasuk.Text = "Masuk";
+            this.buttonMasuk.UseVisualStyleBackColor = false;
+            this.buttonMasuk.Click += new System.EventHandler(this.buttonMasuk_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(187, 257);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(524, 39);
+            this.textBoxPassword.TabIndex = 14;
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPass.Location = new System.Drawing.Point(32, 257);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(130, 36);
+            this.labelPass.TabIndex = 13;
+            this.labelPass.Text = "Password";
+            // 
+            // textBoxUname
+            // 
+            this.textBoxUname.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUname.Location = new System.Drawing.Point(187, 190);
+            this.textBoxUname.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxUname.Name = "textBoxUname";
+            this.textBoxUname.Size = new System.Drawing.Size(524, 39);
+            this.textBoxUname.TabIndex = 12;
+            // 
+            // labelUname
+            // 
+            this.labelUname.AutoSize = true;
+            this.labelUname.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUname.Location = new System.Drawing.Point(24, 193);
+            this.labelUname.Name = "labelUname";
+            this.labelUname.Size = new System.Drawing.Size(138, 36);
+            this.labelUname.TabIndex = 11;
+            this.labelUname.Text = "Username";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(789, 443);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.linkRegister);
+            this.Controls.Add(this.buttonKembali);
+            this.Controls.Add(this.buttonMasuk);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.textBoxUname);
+            this.Controls.Add(this.labelUname);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.LinkLabel linkRegister;
+        private System.Windows.Forms.Button buttonKembali;
+        private System.Windows.Forms.Button buttonMasuk;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.TextBox textBoxUname;
+        private System.Windows.Forms.Label labelUname;
+    }
+}
---
+++ Celikoor_Kelompok6/FormUtama.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Celikoor_Kelompok6
+{
+    partial class FormUtama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.akunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changePasswordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.daftarFilmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.genreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aktorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pegawaiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.konsumenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelJudul = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Tan;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.akunToolStripMenuItem,
+            this.masterToolStripMenuItem,
+            this.adminToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(846, 34);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // akunToolStripMenuItem
+            // 
+            this.akunToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changePasswordToolStripMenuItem,
+            this.logoutToolStripMenuItem});
+            this.akunToolStripMenuItem.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.akunToolStripMenuItem.Name = "akunToolStripMenuItem";
+            this.akunToolStripMenuItem.Size = new System.Drawing.Size(74, 30);
+            this.akunToolStripMenuItem.Text = "Profil";
+            // 
+            // changePasswordToolStripMenuItem
+            // 
+            this.changePasswordToolStripMenuItem.Name = "changePasswordToolStripMenuItem";
+            this.changePasswordToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.changePasswordToolStripMenuItem.Text = "Change Password";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // masterToolStripMenuItem
+            // 
+            this.masterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.daftarFilmToolStripMenuItem,
+            this.genreToolStripMenuItem,
+            this.aktorToolStripMenuItem,
+            this.studioToolStripMenuItem});
+            this.masterToolStripMenuItem.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masterToolStripMenuItem.Name = "masterToolStripMenuItem";
+            this.masterToolStripMenuItem.Size = new System.Drawing.Size(87, 30);
+            this.masterToolStripMenuItem.Text = "Master";
+            // 
+            // daftarFilmToolStripMenuItem
+            // 
+            this.daftarFilmToolStripMenuItem.Name = "daftarFilmToolStripMenuItem";
+            this.daftarFilmToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.daftarFilmToolStripMenuItem.Text = "Films";
+            // 
+            // genreToolStripMenuItem
+            // 
+            this.genreToolStripMenuItem.Name = "genreToolStripMenuItem";
+            this.genreToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.genreToolStripMenuItem.Text = "Genre";
+            // 
+            // aktorToolStripMenuItem
+            // 
+            this.aktorToolStripMenuItem.Name = "aktorToolStripMenuItem";
+            this.aktorToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.aktorToolStripMenuItem.Text = "Aktor";
+            // 
+            // studioToolStripMenuItem
+            // 
+            this.studioToolStripMenuItem.Name = "studioToolStripMenuItem";
+            this.studioToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.studioToolStripMenuItem.Text = "Studio";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pegawaiToolStripMenuItem,
+            this.konsumenToolStripMenuItem});
+            this.adminToolStripMenuItem.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(83, 30);
+            this.adminToolStripMenuItem.Text = "Admin";
+            // 
+            // pegawaiToolStripMenuItem
+            // 
+            this.pegawaiToolStripMenuItem.Name = "pegawaiToolStripMenuItem";
+            this.pegawaiToolStripMenuItem.Size = new System.Drawing.Size(189, 30);
+            this.pegawaiToolStripMenuItem.Text = "Pegawai";
+            // 
+            // konsumenToolStripMenuItem
+            // 
+            this.konsumenToolStripMenuItem.Name = "konsumenToolStripMenuItem";
+            this.konsumenToolStripMenuItem.Size = new System.Drawing.Size(189, 30);
+            this.konsumenToolStripMenuItem.Text = "Konsumen";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Celikoor_Kelompok6.Properties.Resources.logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(215, 184);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelJudul
+            // 
+            this.labelJudul.BackColor = System.Drawing.Color.Beige;
+            this.labelJudul.Font = new System.Drawing.Font("Trebuchet MS", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelJudul.Location = new System.Drawing.Point(0, 56);
+            this.labelJudul.Name = "labelJudul";
+            this.labelJudul.Size = new System.Drawing.Size(801, 116);
+            this.labelJudul.TabIndex = 4;
+            this.labelJudul.Text = "Celikoor 21";
+            this.labelJudul.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormUtama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(846, 605);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelJudul);
+            this.Name = "FormUtama";
+            this.Text = "Celikoor";
+            this.Load += new System.EventHandler(this.FormUtama_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem akunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changePasswordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem daftarFilmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem genreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aktorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pegawaiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem konsumenToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelJudul;
+    }
+}
+
