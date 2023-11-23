4f01d6bc1d7eaa80da1cffe281e7eb841c44df6a
fx surya darmawan <fxsurya27@gmail.com>
2023-11-23 18:50:38 +0700

    NAMBAH LIBRARY

---
+++ Celikoor_LIB/Koneksi.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celikoor_LIB
+{
+    public class Koneksi
+    {
+        private MySqlConnection koneksiDB;
+
+        #region constructors
+        public Koneksi(string pServer, string pDb, string pUID, string pPWD)
+        {
+            string vConnString = "Server=" + pServer + ";Database=" + pDb + ";Uid=" + pUID + ";Pwd=" + pPWD + ";";
+
+            KoneksiDB = new MySqlConnection();
+            KoneksiDB.ConnectionString = vConnString;
+
+            //panggil method buat connect
+            Connect();
+        }
+        #endregion
+
+        #region properties
+        public MySqlConnection KoneksiDB { get => koneksiDB; private set => koneksiDB = value; }
+        #endregion
+
+        #region methods
+        public Koneksi()
+        {
+            Configuration myConf = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            ConfigurationSectionGroup userSetting = myConf.SectionGroups["userSettings"];
+            var settingSection = userSetting.Sections["KuisAkhirSemester_1.qz"] as ClientSettingsSection;
+            string vServer = settingSection.Settings.Get("dbServer").Value.ValueXml.InnerText;
+            string vDb = settingSection.Settings.Get("dbName").Value.ValueXml.InnerText;
+            string vUID = settingSection.Settings.Get("dbUID").Value.ValueXml.InnerText;
+            string vPWD = settingSection.Settings.Get("dbPWD").Value.ValueXml.InnerText;
+
+            string vConnString = "Server=" + vServer + ";Database=" + vDb + ";Uid=" + vUID + ";Pwd=" + vPWD + ";";
+
+            KoneksiDB = new MySqlConnection();
+            KoneksiDB.ConnectionString = vConnString;
+
+            Connect();
+        }
+        public void Connect()
+        {
+            if (KoneksiDB.State == System.Data.ConnectionState.Open)
+                KoneksiDB.Close();
+
+            KoneksiDB.Open();
+        }
+
+        public static MySqlDataReader JalankanPerintahSelect(string sql)
+        {
+            Koneksi k = new Koneksi();
+
+            MySqlCommand c = new MySqlCommand(sql, k.KoneksiDB);
+
+            MySqlDataReader hasil = c.ExecuteReader();
+
+            return hasil;
+        }
+
+        public static int JalankanPerintahNonQuery(string sql)
+        {
+            Koneksi k = new Koneksi();
+
+            MySqlCommand c = new MySqlCommand(sql, k.KoneksiDB);
+
+            int hasil = c.ExecuteNonQuery();
+
+            return hasil;
+        }
+        #endregion
+    }
+}
