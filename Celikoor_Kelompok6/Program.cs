3e9b18b0a1ac022cce14b08276150debdfa8c87c
fx surya darmawan <fxsurya27@gmail.com>
2023-11-24 14:52:24 +0700

    FIX TAHAP 2

--- Celikoor_Kelompok6/Program.cs
+++ Celikoor_Kelompok6/Program.cs
@@ -16,7 +16,7 @@ namespace Celikoor_Kelompok6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormUtama());
+            Application.Run(new FormSplashScreenUtama());
         }
     }
 }
